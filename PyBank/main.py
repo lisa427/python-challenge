cf1c9742b6564d0b8d47f58da375358cd40c0647
lisa427 <lisa.mcgowan4@gmail.com>
2020-09-26 00:32:39 -0400

    prints first month data

--- PyBank/main.py
+++ PyBank/main.py
@@ -4,21 +4,24 @@ import csv
 budget_csv = os.path.join("Resources", "budget_data.csv")
 
 with open(budget_csv) as csv_file:
-    csv_reader = csv.reader(csv_file, delimiter=",")
-   
+    csv_reader = csv.reader(csv_file, delimiter=",")  
     csv_header = next(csv_file)
-
     total = 0
-
     for row in csv_reader:
         total += int(row[1])
 
 with open(budget_csv) as csv_file:
-    csv_reader = csv.reader(csv_file, delimiter=",")
-   
+    csv_reader = csv.reader(csv_file, delimiter=",")  
     csv_header = next(csv_file)
-
     lines = len(list(csv_reader))
+
+with open(budget_csv) as csv_file:
+    r = csv.reader(csv_file)
+    for i in range(1):
+        next(r)
+    row2 = next(r)
+    num = row2[1]
 
 print(lines)
 print(total)
+print(num)
