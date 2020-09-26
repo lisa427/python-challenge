416002628aab8ea0f82c929fd2bcf4da0c6d3412
lisa427 <lisa.mcgowan4@gmail.com>
2020-09-26 01:09:11 -0400

    put changes in a list

--- PyBank/main.py
+++ PyBank/main.py
@@ -22,15 +22,25 @@ with open(budget_csv) as csv_file:
     row2 = next(r)
     num = int(row2[1])
 
+#with open(budget_csv) as csv_file:
+    #csv_reader = csv.reader(csv_file, delimiter=",")  
+    #next(csv_file)
+    #next(csv_file)
+    #for row in csv_reader:
+        #row.append(int(row[1]) - num)
+        #num = int(row[1])
+    #change_list = [row[2]]
+    #print(change_list[3])
+
 with open(budget_csv) as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=",")  
     next(csv_file)
     next(csv_file)
+    change_list = []
     for row in csv_reader:
-        row.append(int(row[1]) - num)
+        change_list.append(int(row[1]) - num)
         num = int(row[1])
-        print(row[2])
 
 print(lines)
 print(total)
-print(num)
+print(change_list)
