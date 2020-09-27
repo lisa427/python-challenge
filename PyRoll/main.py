a2b4695bd9df4326d32d795a1ec22e002388a282
lisa427 <lisa.mcgowan4@gmail.com>
2020-09-26 22:49:25 -0400

    added count

--- PyRoll/main.py
+++ PyRoll/main.py
@@ -6,7 +6,7 @@ election_csv = os.path.join("Resources", "election_data.csv")
 with open(election_csv) as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=",")  
     csv_header = next(csv_file)
-    lines = len(list(csv_reader))
+    vote_count = len(list(csv_reader))
 
 with open(election_csv) as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=",")  
@@ -16,6 +16,27 @@ with open(election_csv) as csv_file:
         if row[2] not in name_list:
             name_list.append(row[2])
 
+#candidate_count = len(name_list)
+#for name in name_list:
 
-print(lines)
+#with open(election_csv) as csv_file:
+    #csv_reader = csv.reader(csv_file, delimiter=",")  
+    #csv_header = next(csv_file)
+    #for row in csv_reader:
+        #for name in name_list:
+            #if name == row[2]:
+
+with open(election_csv) as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter=",")  
+    next(csv_file)
+    votes = []
+    counter = 0
+    for row in csv_reader:
+        votes.append(row[2])
+
+print(votes.count(name_list[counter]))
+    
+
+print(vote_count)
 print(name_list)
+#print(candidate_count)
