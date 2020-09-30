0e048c3aef842dde38aacc75b99e4fa660e626b6
lisa427 <lisa.mcgowan4@gmail.com>
2020-09-29 21:51:55 -0400

    made name lists

--- PyBoss/main.py
+++ PyBoss/main.py
@@ -20,9 +20,23 @@ with open(employee_csv) as csv_file:
 with open(employee_csv) as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=",")  
     next(csv_file)
-    name = []
+    names = []
     for row in csv_reader:
-        name.append(row[1])
+        names.append(row[1])
+
+# creates list of first names
+first_names = []
+for name in names:
+    fname = name.split()
+    fname = fname[0]
+    first_names.append(fname)
+
+# creates list of last names
+last_names = []
+for name in names:
+    lname = name.split()
+    lname = lname[1]
+    last_names.append(lname)
 
 # creates a list from the DOB column from the csv file
 with open(employee_csv) as csv_file:
@@ -48,5 +62,5 @@ with open(employee_csv) as csv_file:
     for row in csv_reader:
         state.append(row[4])
 
-print(name[:10])
-print(state[:10])
+print(first_names[:10])
+print(last_names[:10])
